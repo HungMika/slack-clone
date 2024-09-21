cb8d0808595d0b7ac735bf45cb0b9a0feb262e5a
Hung Dang <106718735+HungMika@users.noreply.github.com>
2024-09-21 11:46:38 +0700

    lint

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,8 +1,8 @@
 import Github from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
 import { convexAuth } from "@convex-dev/auth/server";
-import {Password} from "@convex-dev/auth/providers/Password";
-import {DataModel} from "./_generated/dataModel"
+import { Password } from "@convex-dev/auth/providers/Password";
+import { DataModel } from "./_generated/dataModel";
 
 const CustomPassword = Password<DataModel>({
   profile(params) {
--- convex/users.ts
+++ convex/users.ts
@@ -1,16 +1,16 @@
 import { auth } from "./auth";
 import { getAuthUserId } from "@convex-dev/auth/server";
-import {query} from "./_generated/server";
+import { query } from "./_generated/server";
 
 export const current = query({
-    args: {},
-    handler: async (ctx) => {
-        const userId = await getAuthUserId(ctx);
-        //const userId = await auth.getUserId(ctx);
+  args: {},
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
+    //const userId = await auth.getUserId(ctx);
 
-        if(userId === null){
-            return null;
-        }
-        return await ctx.db.get(userId);
+    if (userId === null) {
+      return null;
     }
-})
+    return await ctx.db.get(userId);
+  },
+});
