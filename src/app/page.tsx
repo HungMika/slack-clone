a750a72bb77e0b02d75105d5e4ef294283e52b15
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-09-21 20:05:07 +0700

    logic for route to exitting work space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import React, { useEffect, useMemo } from "react";
 import { UserButton } from "@/features/auth/components/user-button";
 import { useGetWorkSpaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkSpaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   const [open, setOpen] = useCreateWorkSpaceModal();
 
   const { data, isLoading } = useGetWorkSpaces();
@@ -17,6 +19,7 @@ export default function Home() {
 
     if (workspaceId) {
       console.log("Redirect to workspace");
+      router.replace(`/workspace/${workspaceId}`);
     } else if (!open) {
       setOpen(true);
     }
