405037df10e59c85cd044654fbbf472e0a0726ee
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-09-21 15:28:13 +0700

    add input UI

--- src/features/workspaces/components/create-workspace-model.tsx
+++ src/features/workspaces/components/create-workspace-model.tsx
@@ -7,6 +7,8 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { useCreateWorkspaceAtom } from "../store/use-create-workspace-model";
 
 export const CreateWorkspaceModel = () => {
@@ -17,7 +19,22 @@ export const CreateWorkspaceModel = () => {
   return (
     <Dialog open={open} onOpenChange={handleClose}>
       <DialogContent>
-        <DialogHeader>Add some fking workspaces</DialogHeader>
+        <DialogHeader>
+          <DialogTitle>Add some fking workspaces</DialogTitle>
+        </DialogHeader>
+        <form className="space-y-4" action="submit">
+          <Input
+            value=""
+            disabled={false}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g 'Work', thats all"
+          />
+          <div className="flex justify-end">
+            <Button disabled={false}>Create</Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
