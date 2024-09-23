4bd961a490c4ce20e5158f4f56387b33df840026
Hung Dang <106718735+HungMika@users.noreply.github.com>
2024-09-23 19:58:32 +0700

    prefences modal

---
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -0,0 +1,33 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+
+interface PreferencesModalProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  initialValue: string;
+}
+
+export const PreferencesModal = ({
+  open,
+  setOpen,
+  initialValue,
+}: PreferencesModalProps) => {
+  const [value, setValue] = useState(initialValue);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{value}</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
