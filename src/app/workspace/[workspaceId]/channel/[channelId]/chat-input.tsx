01c3024a580feb7bd8285fafad5c82b618b53357
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-10-24 20:59:21 +0700

    reset the form on submit

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import Quill from "quill";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useCreateMessage } from "@/features/messages/api/use-create-message";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useChannelId } from "@/hooks/use-channel-id";
@@ -14,6 +14,7 @@ interface ChatInputProps {
 }
 
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
+  const [editorKey, setEditorKey] = useState(0);
   const editorRef = useRef<Quill | null>(null);
   // editorRef.current?.focus()
   const workspaceId = useWorkspaceId();
@@ -21,24 +22,34 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
 
   const { mutate: createMessage } = useCreateMessage();
 
-  const handleSubmit = ({
+  const handleSubmit = async ({
     body,
     image,
   }: {
     body: string;
     image: File | null;
   }) => {
-    console.log({ body, image });
-    createMessage({
-      workspaceId,
-      channelId,
-      body,
-    });
+    console.log({ body, image }, "message");
+    try {
+      await createMessage(
+        {
+          workspaceId,
+          channelId,
+          body,
+        },
+        { throwError: true },
+      );
+      setEditorKey((prev) => prev + 1);
+    } catch (e) {
+      console.log(e);
+    } finally {
+    }
   };
 
   return (
     <div className="px-5 w-full">
       <Editor
+        key={editorKey}
         placeholder={placeholder}
         onSubmit={handleSubmit}
         disabled={false}
