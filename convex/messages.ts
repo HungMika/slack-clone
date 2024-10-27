1c0ef38864916d991c3556e9a1a3e52fcb00fc30
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-10-27 14:20:11 +0700

    doccument get member

--- convex/messages.ts
+++ convex/messages.ts
@@ -3,15 +3,24 @@ import { mutation, QueryCtx } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { Id } from "./_generated/dataModel";
 
+const populateUser = (ctx: QueryCtx, userId: Id<"users">) => {
+  return ctx.db.get(userId);
+};
+// just get the info of one member with the id
+const populateMember = (ctx: QueryCtx, memberId: Id<"members">) => {
+  return ctx.db.get(memberId);
+};
+
+// get the memeber of a user in a  workspace
 const getMember = async (
   ctx: QueryCtx,
   workspaceId: Id<"workspaces">,
-  userId: Id<"users">,
+  userId: Id<"users">
 ) => {
   return ctx.db
     .query("members")
     .withIndex("by_workspace_id_user_id", (q) =>
-      q.eq("workspaceId", workspaceId).eq("userId", userId),
+      q.eq("workspaceId", workspaceId).eq("userId", userId)
     )
     .unique();
 };
@@ -22,6 +31,7 @@ export const create = mutation({
     image: v.optional(v.id("_storage")),
     workspaceId: v.id("workspaces"),
     channelId: v.optional(v.id("channels")),
+    conversationId: v.optional(v.id("conversations")),
     parentMessageId: v.optional(v.id("messages")),
     //TODO: add conversationId
   },
@@ -31,6 +41,17 @@ export const create = mutation({
     if (!userId) {
       throw new Error("Unauthorized");
     }
+
+    // Only possible if we are relying in a thread in 1:1 conversations
+    let _conversationId = args.conversationId;
+    if (!args.conversationId && !args.channelId && args.parentMessageId) {
+      const parentMessage = await ctx.db.get(args.parentMessageId);
+      if (!parentMessage) {
+        throw new Error("Parent message not found");
+      }
+      _conversationId = parentMessage.conversationId;
+    }
+
     const member = await getMember(ctx, args.workspaceId, userId);
     if (!member) {
       throw new Error("Unauthorized");
@@ -41,6 +62,7 @@ export const create = mutation({
       body: args.body,
       image: args.image,
       channelId: args.channelId,
+      conversationId: _conversationId,
       workspaceId: args.workspaceId,
       parentMessageId: args.parentMessageId,
       updatedAt: Date.now(),
