9ab780587e41b0fc02037d9369ca8983b13c04f5
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-11-04 22:31:15 +0700

    add model to the scene

---
+++ src/app/3dPlayground/[id]/layout.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Player from "@/components/3d/player-main";
+
+export default function Page() {
+  return (
+    <div style={{ height: "100%", width: "100%" }}>
+      <Player />
+    </div>
+  );
+}
