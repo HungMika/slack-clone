34a1d3ab033fed6397c29b10f5d0b7421c2b7cb5
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-11-10 13:01:20 +0700

    refactor

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -25,22 +25,26 @@ export const Toolbar = ({
   // refactor code to use array of object
   const allButton = [
     {
+      isShow: true,
       label: "Reaction",
       icon: <Smile className="size-4" />,
       hoverOption: "popphover",
       onClick: handleReaction,
     },
     {
+      isShow: !hideThreadButton,
       label: "Edit",
       icon: <Pencil className="size-4" />,
       onClick: handleEdit,
     },
     {
+      isShow: !hideThreadButton,
       label: "Delete",
       icon: <Trash className="size-4" />,
       onClick: handleDelete,
     },
     {
+      isShow: !hideThreadButton,
       label: "Thread",
       icon: <MessageSquareTextIcon className="size-4" />,
       onClick: handleThread,
@@ -55,14 +59,14 @@ export const Toolbar = ({
             <Smile className="size-4" />,
           </Button>
         </EmojiPopover>
-
-        <Hint label={`edit messages`}>
-          <Button variant={"ghost"} size={"iconSm"} disabled={isPending}>
-            <MessageSquareTextIcon className="size-4" />
-          </Button>
-        </Hint>
-
         {!hideThreadButton && (
+          <Hint label={`edit messages`}>
+            <Button variant={"ghost"} size={"iconSm"} disabled={isPending}>
+              <MessageSquareTextIcon className="size-4" />
+            </Button>
+          </Hint>
+        )}
+        {isAuthor && (
           <Hint label={`edit messages`}>
             <Button variant={"ghost"} size={"iconSm"} disabled={isPending}>
               <Pencil className="size-4" />
@@ -76,6 +80,34 @@ export const Toolbar = ({
             </Button>
           </Hint>
         )}
+        {/* {allButton.map((button, index) => {
+          if (button.isShow) {
+            if (button.hoverOption === "popphover") {
+              return (
+                <EmojiPopover
+                  onEmojiSelect={(emoji) => handleReaction(emoji.native)}
+                >
+                  <Button
+                    variant={"ghost"}
+                    size={"iconSm"}
+                    disabled={isPending}
+                  >
+                    {button.icon}
+                  </Button>
+                </EmojiPopover>
+              );
+            }
+            return (
+              <Hint label={button.label} key={index}>
+                <Button variant={"ghost"} size={"iconSm"} disabled={isPending}>
+                  {button.icon}
+                </Button>
+              </Hint>
+            );
+          }
+        })} 
+        // another option
+        */}
       </div>
     </div>
   );
