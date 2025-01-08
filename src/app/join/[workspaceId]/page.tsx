292e5ca775390f8d3a864c63db4618dfc447d058
Hung Dang <106718735+HungMika@users.noreply.github.com>
2025-01-08 18:39:03 +0700

    logo aliens.png

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -52,7 +52,7 @@ const JoinPage = () => {
 
   return (
     <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-md">
-      <Image src="/logo.svg" width={60} height={60} alt="Logo" />
+      <Image src="/aliens.png" width={60} height={60} alt="Logo" />
       <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
         <div className="flex flex-col gap-y-2 items-center justify-center">
           <h1 className="text-2xl font-bold">Join {data?.name}</h1>
