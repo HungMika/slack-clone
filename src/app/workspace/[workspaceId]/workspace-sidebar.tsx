2d780ae8c4ea1b6aebda01fdb32431dc77188c05
Hung Dang <106718735+HungMika@users.noreply.github.com>
2025-01-13 10:57:23 +0700

    notifications-workspace-sidebar-fix

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -20,12 +20,16 @@ import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkSpace } from "@/features/workspaces/api/use-get-workspace";
 import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
+import { useSearchParams } from "next/navigation";
+import { useGetUserHistory } from "@/features/messages/api/use-get-user-history";
+import { useCurrentUser } from "@/features/auth/api/use-current-user";
 
 export const WorkSpaceSideBar = () => {
+  const allnotifications = useGetUserHistory();
   const memberId = useMemberId();
   const channelId = useChannelId();
   const workspaceId = useWorkspaceId();
-
+  const currentUser = useCurrentUser();
   const [_isOpen, setIsOpen] = useCreateChannelModal();
 
   const { data: channels, isLoading: channelsLoading } = useGetChannels({
@@ -40,7 +44,56 @@ export const WorkSpaceSideBar = () => {
   const { data: members, isLoading: membersLoading } = useGetMember({
     workspaceId,
   });
+  const handleFilterNotifications = ({ ChannelId }: { ChannelId: string }) => {
+    // NOTE(Khang): this array have all the info of all message related to the current user
+    // NOTE(Khang): get the memberID of the user in the channel then from that we can filter out the notifiations
+    const memberInChannel = allnotifications?.data?.members.find(
+      (member) => member.userId === currentUser.data?._id
+    );
 
+    const filteredNotifications = allnotifications?.data?.allConversationInfo?.filter(
+      (mess) => {
+        const checkIsinChannel = mess.channelId === ChannelId;
+        let checkIfMemberSeen = false;
+        if (memberInChannel && currentUser.data) {
+          checkIfMemberSeen =
+            mess?.seenMembers?.includes(currentUser?.data?._id) ?? false;
+        }
+        return checkIsinChannel && checkIfMemberSeen;
+      }
+    );
+    return filteredNotifications?.length;
+  };
+  // console.log("debug conser info: ", allnotifications?.data?.arrayWithConversationInfo);
+  const handleFilterConvertations = ({
+    UserId,
+  }: {
+    UserId: string;
+  }) => {
+    const converationInfo = allnotifications?.data?.allConversationInfo?.find(item=>{
+      if(!item.conversationInfo) return false
+      if(!item.conversationInfo.memberOne?.length) return false
+      if(!item.conversationInfo.memberTwo?.length) return false
+      return item.conversationInfo?.memberOne[0]?.userId === UserId || item.conversationInfo?.memberTwo[0].userId===UserId
+    })
+
+    if(!converationInfo) return 0
+
+    const filteredNotifications = allnotifications?.data?.allConversationInfo?.filter(
+      (mess) => {
+        const checkIsCon= mess.conversationId === converationInfo.conversationId;
+        let checkIfMemberSeen = false;
+        if (currentUser.data) {
+          checkIfMemberSeen =
+            mess?.seenMembers?.includes(currentUser?.data?._id) ?? false;
+        }
+        return checkIsCon && checkIfMemberSeen
+      }
+    );
+    
+    
+    return filteredNotifications?.length;
+  }
   if (memberLoading || workspaceLoading) {
     return (
       <div className="flex flex-col bg-[#5e2c5f] h-full items-center justify-center">
@@ -80,6 +133,7 @@ export const WorkSpaceSideBar = () => {
             icon={HashIcon}
             label={item.name}
             variant={channelId === item._id ? "active" : "default"}
+            notifications={handleFilterNotifications({ ChannelId: item._id })}
           />
         ))}
       </WorkspaceSection>
@@ -96,10 +150,11 @@ export const WorkSpaceSideBar = () => {
               image={item.user.image}
               label={item.user.name}
               variant={item._id === memberId ? "active" : "default"}
+              notifications = {item.userId === currentUser.data?._id? 0 : handleFilterConvertations({ UserId: item.user._id })}
             />
           </div>
         ))}
       </WorkspaceSection>
     </div>
-  );
+  )
 };
